4254898bb79af8cf6ed494938a765315120b35e5
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2023-06-09 20:53:17 -0300

    Conexao com banco de dados

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+
+define ('DB_USER', "ECLBDIT203");
+define ('DB_PASSWORD', "@jxD9IKa!001");
+define ('DB_DATABASE', "bdengcomp_high");
+
+$conn = oci_connect(DB_USER, DB_PASSWORD, DB_DATABASE);
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'SELECT * FROM employees');
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
