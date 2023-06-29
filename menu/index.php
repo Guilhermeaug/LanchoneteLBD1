267b4d52a5eefac9d6172bd77f85c21264c6e953
Guilherme Augusto <guilhermeaugustodeoliveira66@gmail.com>
2023-06-29 17:44:31 -0300

    update menu

--- menu/index.php
+++ menu/index.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-  <meta charset="UTF-8" />
-  <title>Dashboard</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
-  <link href="./style.css" rel="stylesheet"/>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <meta charset="UTF-8" />
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+    <link href="./style.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div class="container">
-    <?php
-    include'../partials/Header.php';
-    ?>
         <h1 class="text-uppercase font-weight-bold center">MENU DE OPERAÇÕES</h1>
+        <div class="d-grid">
+            <a class="btn btn-primary" href="/lanchonete/stats">Página de estatísticas</a>
+        </div>
         <div>
-        <table class="table">
-                <tr >
-                    <th class="center"> <h2>CADASTRAR</h2></th>
+            <table class="table">
+                <tr>
+                    <th class="center">
+                        <h2>CADASTRAR</h2>
+                    </th>
                 </tr>
                 <tr>
-                    <td class="center" ><a class="link" href="/lanchonete/produto">Produto</a></td>
+                    <td class="center"><a class="link" href="/lanchonete/produto">Produto</a></td>
                 </tr>
                 <tr>
                     <td class="center"><a class="link" href="/lanchonete/fornecedor">Fornecedor</a></td>
@@ -32,10 +36,12 @@
                 </tr>
             </table>
         </div>
-        <div> 
+        <div>
             <table class="table ">
                 <tr class="linha">
-                    <th class="center"> <h2>EDITAR</h2></th>
+                    <th class="center">
+                        <h2>EDITAR</h2>
+                    </th>
                 </tr>
                 <tr>
                     <td class="center"><a class="link" href="/lanchonete/listaEditProdutos">Produto</a></td>
@@ -51,10 +57,12 @@
                 </tr>
             </table>
         </div>
-        <div> 
+        <div>
             <table class="table ">
                 <tr class="linha">
-                    <th class="center"> <h2>EXCLUIR</h2></th>
+                    <th class="center">
+                        <h2>EXCLUIR</h2>
+                    </th>
                 </tr>
                 <tr>
                     <td class="center"><a class="link" href="/lanchonete/listaDelProduto">Produto</a></td>
@@ -70,6 +78,7 @@
                 </tr>
             </table>
         </div>
-        
+
 </body>
+
 </html>
